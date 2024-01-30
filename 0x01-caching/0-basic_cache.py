3abc2593d37cca0e634d9096c33e0279632d701b
elyse502 <elyseniyibizi502@gmail.com>
2024-01-30 08:01:37 +0000

    Return the value in self.cache_data linked to key

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python Module"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Dictionary"""
+
+    def put(self, key, item):
+        """Puts item in cache"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Gets item from cache"""
+        return self.cache_data.get(key, None)
