95984d06deb3ff55601824a3bf7b392fee7a9de0
elyse502 <elyseniyibizi502@gmail.com>
2024-01-30 08:11:45 +0000

    Add solution to task 4. MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Python Module """
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRUCahce class"""
+
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """Put in cache"""
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            if self.queue:
+                last = self.queue.pop()
+                del self.cache_data[last]
+                print("DISCARD: {}".format(last))
+
+        if key not in self.queue:
+            self.queue.append(key)
+        else:
+            self.mv_last_list(key)
+
+    def get(self, key):
+        """Get form cache"""
+        item = self.cache_data.get(key, None)
+        if item is not None:
+            self.mv_last_list(key)
+        return item
+
+    def mv_last_list(self, item):
+        """Moves element"""
+        length = len(self.queue)
+        if self.queue[length - 1] != item:
+            self.queue.remove(item)
+            self.queue.append(item)
