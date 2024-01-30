6b8029ae8057485316f01311099ece48b7e22824
elyse502 <elyseniyibizi502@gmail.com>
2024-01-30 08:07:28 +0000

    Add solution to task 2. LIFO Caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Python Module """
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFOCache class"""
+
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """Put in cache"""
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            if self.queue:
+                last = self.queue.pop()
+                del self.cache_data[last]
+                print("DISCARD: {}".format(last))
+
+        if key not in self.queue:
+            self.queue.append(key)
+        else:
+            self.mv_last_list(key)
+
+    def get(self, key):
+        """Get form cache"""
+        return self.cache_data.get(key, None)
+
+    def mv_last_list(self, item):
+        """Moves element"""
+        length = len(self.queue)
+        if self.queue[length - 1] != item:
+            self.queue.remove(item)
+            self.queue.append(item)
