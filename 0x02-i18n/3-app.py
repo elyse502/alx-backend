f2ce41446483915f1ef1cda4f13cf7667fa2a719
elyse502 <elyseniyibizi502@gmail.com>
2024-02-06 10:31:18 +0200

    Add solution to Task 3. Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,34 +1,43 @@
 #!/usr/bin/env python3
 """ Python Module """
+from flask import Flask, render_template, request
 from flask_babel import Babel
-from flask import Flask, render_template
-
-app = Flask(__name__, template_folder='templates')
-babel = Babel(app)
 
 
 class Config(object):
-    """ Class for Babel """
+    """_summary_
 
+    Returns:
+            _type_: _description_
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# configure the flask app
+app = Flask(__name__)
 app.config.from_object(Config)
-
-
-@app.route('/', methods=['GET'], strict_slashes=False)
-def hello_world() -> str:
-    """ Basic Template for Babel Implementation"""
-    return render_template("3-index.html")
+app.url_map.strict_slashes = False
+babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """Select a language to translate """
+def get_locale():
+    """_summary_
+
+    Returns:
+            _type_: _description_
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/')
+def index():
+    """_summary_
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
